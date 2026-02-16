0695fef2b38bde1adf27d52892586dad2ec6fcb6
Juan Vicente Gonzalez <juan.vicente.gonzalez93@gmail.com>
2026-02-16 14:46:10 -0300

    Se implementa minimax

--- gato_y_raton2.0.py
+++ gato_y_raton2.0.py
@@ -2,8 +2,7 @@ import random
 
 tamano_x = 5            #Tamano i de la matriz
 tamano_y = 5            #Tamano j de la matriz
-cant_turno = 50
-#tablero = []            #Tablero de juego
+cant_turno = 50         #Cantidad de turnos
 
 def crear_tablero():
     tablero = []
@@ -70,13 +69,24 @@ def mover_ficha(mov, ficha, pos,tablero_de_juego):
 
     return i,j 
 
+def mover_ficha_sim(mov, ficha, pos):
+    i = pos[0]
+    j = pos[1]
+
+    if mov == "w": #Mueve las piezas, arriba
+        i -= 1
+    elif mov == "s": #Mueve las piezas, abajo
+        i += 1
+    elif mov == "d": #Mueve las piezas, derecha
+        j += 1
+    elif mov == "a": #Mueve las piezas, izquierda
+        j -= 1
+    return i,j 
+
 def corroborar_fin(cant_turno, pos_1,  pos_2, tablero_de_juego):
     if  cant_turno == 0:
-        print("El RATON se escapo")
         return True
-    elif pos_1 == pos_2:
-        tablero_de_juego[pos_1[0]][pos_1[1]] = "G"
-        print("El GATO comio al raton")        
+    elif pos_1 == pos_2:  
         return True
     else:
         return False
@@ -88,11 +98,57 @@ def cambiar_turno(turno_actual):
         turno_actual = "G"
     return turno_actual
 
-#faltan ajustar los parametros
-def dit_manhatan(pos_gato,pos_raton):
-
+def dist_manhatan(pos_gato,pos_raton):
     dist_manh = abs(pos_gato[0]-pos_raton[0]) + abs(pos_gato[1]-pos_raton[1])
     return dist_manh
+
+def minimax(pos_gato,pos_raton,cant_turno, profundidad,maximizador):
+    movimientos = ["w","s","a","d"]
+
+    if profundidad == 0 or corroborar_fin(cant_turno, pos_gato, pos_raton,tablero_de_juego):
+        return -dist_manhatan(pos_gato, pos_raton)
+    
+    if maximizador:
+        maximo = -99999
+        for movimiento in movimientos:
+            if movimineto_valido(movimiento, pos_gato):
+                pos_gato_sim = list(mover_ficha_sim(movimiento, "G", pos_gato))
+                aux_max = minimax(pos_gato_sim, pos_raton, cant_turno - 1, profundidad - 1, False)
+                maximo = max(aux_max, maximo)
+        return maximo
+    else:
+        minimo= 99999
+        for movimiento in movimientos:
+            if movimineto_valido(movimiento, pos_raton):
+                pos_raton_sim = list(mover_ficha_sim(movimiento, "R", pos_raton))
+                aux_min = minimax(pos_gato, pos_raton_sim, cant_turno - 1, profundidad - 1, True)
+                minimo = min(aux_min, minimo)
+        return minimo
+
+def fun_de_mov_ai(turno_actual, pos_gato, pos_raton):
+    movimientos = ["w","s","a","d"]
+    mejor_mov = ""
+
+    if turno_actual == "G":
+        mejor_puntaje = -99999
+        for movimiento in movimientos:
+            if movimineto_valido(movimiento, pos_gato):
+                pos_gato_sim = mover_ficha_sim (movimiento,"G", pos_gato) 
+                puntaje = minimax(pos_gato_sim, pos_raton, cant_turno, 3, False)
+                if puntaje > mejor_puntaje:
+                    mejor_puntaje = puntaje
+                    mejor_mov = movimiento
+    else:
+        mejor_puntaje = 99999
+        for movimiento in movimientos:
+            if movimineto_valido(movimiento, pos_raton):
+                pos_raton_sim = mover_ficha_sim (movimiento,"R", pos_raton) 
+                puntaje = minimax(pos_gato, pos_raton_sim, cant_turno, 3, True)
+                if puntaje < mejor_puntaje:
+                    mejor_puntaje = puntaje
+                    mejor_mov = movimiento
+
+    return mejor_mov
 
 tablero_de_juego = crear_tablero()
 print("Juego del Gato y Raton")
@@ -132,9 +188,19 @@ while True:
 
     else:
         print("Turno de la IA")
-
+        mov_ai = fun_de_mov_ai(turno_actual,pos_gato,pos_raton)
+        if turno_actual == "G":
+            pos_gato = list(mover_ficha(mov_ai, "G", pos_gato, tablero_de_juego))
+        else:
+            pos_raton = list(mover_ficha(mov_ai, "R", pos_raton,tablero_de_juego))
+        cant_turno -= 1
         turno_actual = cambiar_turno(turno_actual)
     
-    if corroborar_fin(cant_turno, pos_gato, pos_raton,tablero_de_juego):
+    if corroborar_fin(cant_turno, pos_gato, pos_raton, tablero_de_juego):
+        if  pos_gato == pos_raton:
+            tablero_de_juego[pos_gato[0]][pos_gato[1]] = "G"
+            print("El GATO comio al raton")  
+        if cant_turno == 0:
+             print("El RATON se escapo")    
         imprimir_tablero(tablero_de_juego)
         break
